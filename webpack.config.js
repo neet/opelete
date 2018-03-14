3bb55037bdb0adb9ca358725a24ef9ebbaf4b27e
Neetshin <neetshin@neetsh.in>
2018-03-14 10:05:17 +0530

    Add cleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-const path    = require('path');
+const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const config = {
 
@@ -52,6 +53,10 @@ const config = {
   },
 
   plugins: [
+    new CleanWebpackPlugin([
+      'dist',
+    ]),
+
     new ExtractTextPlugin({
       filename: '[name].css',
       allChunks: true,
