6c221717cf37823fe6d4c80a4d43e82d3c21d8ee
neet <n33t5hin@gmail.com>
2018-08-23 00:54:19 +0900

    Update snapshot

--- src/popup/components/__tests__/Checkbox-test.tsx
+++ src/popup/components/__tests__/Checkbox-test.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
-import Checkbox from '../Checkbox';
-import renderer from 'react-test-renderer';
+import * as renderer from 'react-test-renderer';
+import { Checkbox } from '../Checkbox';
 
 describe('Checkbox', () => {
   it('renders a checkbox', () => {
     const handler = jest.fn();
     const tree = renderer.create(
       <Checkbox
-        className='foo'
-        value={false}
+        checked={false}
         text='hoge'
         onChange={handler}
-      />
+      />,
     ).toJSON();
 
     expect(tree).toMatchSnapshot();
